1379ca651a5ab05beb31184c54b7735e6cd81c28
161250064 <161250064@smail.nju.edu.cn>
2018-06-20 23:59:53 +0800

    聚类接口

---
+++ src/main/java/cts/phase3/controller/utils/ClusteringUtil.java
@@ -0,0 +1,14 @@
+package cts.phase3.controller.utils;
+
+import java.util.List;
+
+public class ClusteringUtil {
+
+    /*
+        格式为xl1_yl1_xr1_yr1_xl2_yl2_xr2_yr2
+     */
+    public String clusteringXY(List<String> stringList){
+
+        return "";
+    }
+}
